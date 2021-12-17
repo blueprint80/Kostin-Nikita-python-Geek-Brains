f4e178db6afd427bece2ec63878bb95c83097554
blueprint80 <95220602+blueprint80@users.noreply.github.com>
2021-12-17 17:25:17 +0300

    Delete lesson2 task4.py

--- lesson2 task4.py
+++
@@ -1,6 +0,0 @@
-lst = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-for element in lst:
-    job_name = element.split(' ')[::-1]
-    first_el = job_name[0]
-    name = first_el.lower().title()
-    print('Привет', name + "!")
